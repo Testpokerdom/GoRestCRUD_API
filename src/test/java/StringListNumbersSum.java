72005708fe3846008fb029b9590e1544940fcd0b
ViktorBibik <viktorbibik1988@gmail.com>
2021-04-16 18:50:19 +0300

    Numbers count class was added

---
+++ src/test/java/StringListNumbersSum.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class StringListNumbersSum {
+    static double firstValue;
+    static String digitalValueFromList;
+    static double sumFirstAndNext;
+
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("test.csv");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        while ((digitalValueFromList = reader.readLine()) != null){
+
+            if (digitalValueFromList.isEmpty() || digitalValueFromList.equals(null) || digitalValueFromList.contains("#")){
+                continue;
+            }
+
+            firstValue = Double.parseDouble(digitalValueFromList);
+            sumFirstAndNext += firstValue;
+            System.out.println(sumFirstAndNext);
+
+        }
+    }
+}
